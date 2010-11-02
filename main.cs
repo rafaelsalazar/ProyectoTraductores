13b875903a77dec5db4c60e8c83c0ff5b077f77c
Javier Ayala <jav.forzza@gmail.com>
2010-11-02 12:52:35 -0600

    Agregue archivo main

---
+++ main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace proyectoOO
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string archivo="";
+            while (archivo != "salir")
+            {
+                Console.WriteLine("Introduzca nombre del archivo: ");
+                archivo = Console.ReadLine();
+                if (archivo != "salir")
+                {
+                    Scanner scanner = new Scanner(archivo);
+                    Parser parser = new Parser(scanner);
+                    parser.Parse();
+                    
+                }
+            }
+       }
+    }
+}
